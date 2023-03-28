89a2390b2ab8c774e3064eaaca700b037b7f48ab
Umasuthan Ramakrishnan <umasuthan@innoventes.co>
2023-03-28 18:31:12 +0530

    Corrected class names and filenames. Fixes issue #5

--- src/main/java/com/innoventes/test/app/InnoventesSpringbootTestApplication.java
+++ src/main/java/com/innoventes/test/app/InnoventesSpringbootTestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InnotentesSpringbootTestApplication {
+public class InnoventesSpringbootTestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InnotentesSpringbootTestApplication.class, args);
+		SpringApplication.run(InnoventesSpringbootTestApplication.class, args);
 	}
 
 }
--- src/test/java/com/innoventes/test/app/InnotentesSpringbootTestApplicationTests.java
+++ src/test/java/com/innoventes/test/app/InnotentesSpringbootTestApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class InnotentesSpringbootTestApplicationTests {
+class InnoventesSpringbootTestApplicationTests {
 
 	@Test
 	void contextLoads() {
